d015a01e92a86d2f0ddb4ecf9a79ae044524c9a7
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-06-10 23:30:53 +0200

    auth feedback msgs and actions

---
+++ src/Security/LoginSuccessHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    public function __construct(
+        private readonly UrlGeneratorInterface $urlGenerator,
+    ) {
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): ?Response
+    {
+        $roles = $token->getRoleNames();
+
+        if (in_array('ROLE_ADMIN', $roles, true) || in_array('ROLE_SUPER_ADMIN', $roles, true)) {
+            return new RedirectResponse($this->urlGenerator->generate('admin_dashboard'));
+        }
+
+        if (in_array('ROLE_HOST', $roles, true)) {
+            return new RedirectResponse($this->urlGenerator->generate('app_host_dashboard'));
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+    }
+}
