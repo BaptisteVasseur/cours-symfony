f541871696a788768fdaf594575f8a210a51b8b9
Mohamed Dioume <mohamed@MacBook-Pro-de-Mohamed.local>
2026-06-11 10:28:39 +0200

    feat: add async email notifications with Messenger

---
+++ src/Message/ReservationConfirmedMessage.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message;
+
+class ReservationConfirmedMessage
+{
+    public function __construct(
+        public readonly string $reservationId,
+    ) {
+    }
+}
