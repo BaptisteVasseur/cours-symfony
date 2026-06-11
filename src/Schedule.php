27ca38ae23a32967962479bd0f3bab357aea6560
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-06-11 14:51:14 +0200

    symfony Scheduler to run every 15 min to cancel pending after 24

--- src/Schedule.php
+++ src/Schedule.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use Symfony\Component\Console\Messenger\RunCommandMessage;
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
+use Symfony\Component\Scheduler\RecurringMessage;
 use Symfony\Component\Scheduler\Schedule as SymfonySchedule;
 use Symfony\Component\Scheduler\ScheduleProviderInterface;
 use Symfony\Contracts\Cache\CacheInterface;
@@ -20,9 +22,9 @@ class Schedule implements ScheduleProviderInterface
         return (new SymfonySchedule())
             ->stateful($this->cache) // ensure missed tasks are executed
             ->processOnlyLastMissedRun(true) // ensure only last missed task is run
-
-            // add your own tasks here
-            // see https://symfony.com/doc/current/scheduler.html#attaching-recurring-messages-to-a-schedule
+            ->add(
+                RecurringMessage::every('15 minutes', new RunCommandMessage('app:booking:expire'))
+            )
         ;
     }
 }
