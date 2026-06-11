09a51d80015b77be9f66921bf5c5f0c95917824c
yasminebadarou.yb <yasminebadarou.yb@gmail.com>
2026-06-11 23:58:42 +0200

    relecture migrations

--- migrations/Version20260611083217.php
+++ migrations/Version20260611083217.php
@@ -19,7 +19,6 @@ final class Version20260611083217 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE properties ADD calendar_token VARCHAR(64) DEFAULT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_87C331C73363A255 ON properties (calendar_token)');
         $this->addSql('ALTER TABLE users ALTER roles DROP DEFAULT');
@@ -27,7 +26,6 @@ final class Version20260611083217 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP INDEX UNIQ_87C331C73363A255');
         $this->addSql('ALTER TABLE properties DROP calendar_token');
         $this->addSql('ALTER TABLE users ALTER roles SET DEFAULT \'[]\'');
