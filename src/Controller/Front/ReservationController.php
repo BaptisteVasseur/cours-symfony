5754b1dbbd00523bb349329de1fe2c48cb7acb1c
WilliammCorreia <william.correia@edu.ece.fr>
2026-06-11 11:09:18 +0200

    feat: Ajoute la réservation fonctionnelle

--- src/Controller/Front/ReservationController.php
+++ src/Controller/Front/ReservationController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Front;
 use App\Entity\Reservation;
 use App\Entity\User;
 use App\Repository\ReservationRepository;
+use App\Service\BookingService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -32,17 +33,25 @@ final class ReservationController extends AbstractController
 
     #[Route('/{id}', name: 'app_reservation_show', methods: ['GET'])]
     #[IsGranted(ReservationVoter::VIEW, subject: 'reservation')]
-    public function show(Reservation $reservation, ReservationRepository $reservationRepository): Response
-    {
+    public function show(
+        Reservation $reservation,
+        ReservationRepository $reservationRepository,
+        BookingService $bookingService,
+    ): Response {
         $user = $this->getUser();
         if (!$user instanceof User) {
             return $this->redirectToRoute('app_login');
+        }
+
+        if ($bookingService->expireStalePending($reservation)) {
+            $this->addFlash('error', 'Le délai de paiement de 15 minutes est dépassé : la réservation a été annulée et les dates libérées.');
         }
 
         $reservation = $reservationRepository->findOneForDetail($reservation) ?? $reservation;
 
         return $this->render('front/reservation/show.html.twig', [
             'reservation' => $reservation,
+            'paymentDeadline' => $bookingService->paymentDeadline($reservation),
         ]);
     }
 }
